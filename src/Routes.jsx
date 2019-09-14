77e34d5bd59f759f7a1f846824917096eec5b472
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-09-14 11:27:59 -0300

    Update routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,8 +5,8 @@ import Chat from './containers/chat/Chat'
 
 export default () =>
   <BrowserRouter>
-    <Switch name="http://localhost:3000">
-      <Route path="/" component={Signin} />
-      <Route path="/chat" exact component={Chat} />
+    <Switch name='http://localhost:3000'>
+      <Route path='/chat' exact component={Chat} />
+      <Route path='/' component={Signin} />
     </Switch>
   </BrowserRouter>
