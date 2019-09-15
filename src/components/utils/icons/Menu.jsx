bb2388ae724cbc94dd5f4f29be0dc0ffafa22f3b
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-09-15 16:32:57 -0300

    Adds menu icon

---
+++ src/components/utils/icons/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Menu = props => {
+  const IconStyle = { fill: props.color || "#000000" }
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path style={IconStyle} d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+    </svg>
+  )
+}
