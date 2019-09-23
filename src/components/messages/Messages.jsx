88cdfd12795588d4398abfaaf4198b0a2eb62276
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-09-22 21:15:31 -0300

    Fixed bottom scroll

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -2,17 +2,27 @@ import React from 'react'
 import TextBox from '../utils/textbox/TextBox'
 import './messages.css'
 
-export default props =>
-  <div className="chat-messages">
-    {props.messages.map((message, i) =>
-      <TextBox
-        key={i}
-        author={message.author}
-        text={message.message}
-        type={(message.author !== props.username) ? '' : 'sender'}
-        bgColor={(message.author !== props.username) ? '' : '#0080FF'}
-        color={(message.author !== props.username) ? '' : '#FFFFFF'}
-        userColor={message.color}
-      />
-    )}
-  </div>
+export default class extends React.Component {
+  componentDidUpdate() {
+    const $div = document.querySelector('.chat-messages')
+    $div.scrollTop = $div.scrollHeight
+  }
+
+  render() {
+    return (
+      <div className="chat-messages">
+        {this.props.messages.map((message, i) =>
+          <TextBox
+            key={i}
+            author={message.author}
+            text={message.message}
+            type={(message.author !== this.props.username) ? '' : 'sender'}
+            bgColor={(message.author !== this.props.username) ? '' : '#0080FF'}
+            color={(message.author !== this.props.username) ? '' : '#FFFFFF'}
+            userColor={message.color}
+          />
+        )}
+      </div>
+    )
+  }
+}
